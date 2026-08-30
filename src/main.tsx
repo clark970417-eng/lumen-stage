40a06fee795b4e3fcfddbf82fc547f288615bddf
Clark <301422770+clark970417-eng@users.noreply.github.com>
2026-08-30 12:51:03 +0800

    feat: make every interface viewport-safe

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './guide.css'
+import './responsive.css'
 import Root from './Root'
 import { startErrorMonitoring } from './monitoring'
 
